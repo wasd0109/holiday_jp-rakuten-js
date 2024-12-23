b40d967cf28246ba410629fc399f51833c79be2a
Cheung, Kin Ching | Ken | BSD <kinching.a.cheung@rakuten.com>
2024-12-23 18:32:15 +0900

    update test

--- test/holidays.js
+++ test/holidays.js
@@ -1,27 +1,31 @@
-var { describe, it } = require("node:test")
-var { strictEqual } = require("node:assert")
+var { describe, it } = require("node:test");
+var { strictEqual } = require("node:assert");
 var yaml = require('js-yaml');
 var holidays = require('./../lib/holidays');
 
 var HOLIDAYS_DETAIL_URL = 'https://raw.githubusercontent.com/holiday-jp/holiday_jp/master/holidays_detailed.yml';
+var RAKUTEN_HOLIDAYS_DETAIL_URL = 'https://raw.githubusercontent.com/wasd0109/holiday_jp-rakuten-js/refs/heads/master/rakuten_holidays_detailed.yml';
 
-describe('holidays', function(){
-  it('holidays should be holiday all holidays_detailed.yml', function(_, done){
+describe('holidays', function () {
+  it('holidays should be holiday all holidays_detailed.yml', function (_, done) {
     fetch(HOLIDAYS_DETAIL_URL)
       .then(res => res.text())
-      .then(body => {
-        var testset = yaml.safeLoad(body);
-        function format(date) {
-          var year = date.getFullYear();
-          var month = ('0' + (date.getMonth() + 1)).slice(-2);
-          var day = ('0' + (date.getDate())).slice(-2);
-          return (year + '-' + month + '-' + day);
-        }
-        Object.keys(testset).forEach(function (key) {
-          var d = format(new Date(key));
-          strictEqual(holidays[d].name, testset[key].name);
+      .then(jpHolidaysBody => {
+        fetch(RAKUTEN_HOLIDAYS_DETAIL_URL).then(res => res.text()).then(rakutenHolidaysBody => {
+          var jpTestset = yaml.safeLoad(jpHolidaysBody);
+          var rakutenTestset = yaml.safeLoad(rakutenHolidaysBody);
+          function format(date) {
+            var year = date.getFullYear();
+            var month = ('0' + (date.getMonth() + 1)).slice(-2);
+            var day = ('0' + (date.getDate())).slice(-2);
+            return (year + '-' + month + '-' + day);
+          }
+          Object.keys(testset).forEach(function (key) {
+            var d = format(new Date(key));
+            strictEqual(holidays[d].name, testset[key].name);
+          });
+          done();
         });
-        done();
       });
   });
 });
