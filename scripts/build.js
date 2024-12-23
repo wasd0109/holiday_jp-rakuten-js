de054563c6e8f7626f85d9b3260fb1a6747c5b23
Cheung, Kin Ching | Ken | BSD <kinching.a.cheung@rakuten.com>
2024-12-23 17:55:48 +0900

    change build setting

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,10 @@
 var fs = require('fs')
-, util = require('util');
+    , util = require('util');
 
 var header = fs.readFileSync(__dirname + '/header.js', 'utf8');
 var footer = fs.readFileSync(__dirname + '/footer.js', 'utf8');
 
-var data = fs.readFileSync(__dirname + '/holiday_jp_webpacked.js', 'utf8');
+var data = fs.readFileSync(__dirname + '/rakuten_holiday_jp_webpacked.js', 'utf8');
 
-fs.writeFileSync(__dirname + '/../release/holiday_jp.js', header + data + footer);
+fs.writeFileSync(__dirname + '/../release/rakuten_holiday_jp.js', header + data + footer);
 
